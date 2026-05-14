8f34901799f34c07e712c5a4c9aa9110c8235241
BrandonMiguel <bespino@ipte.com.mx>
2026-05-14 10:02:27 -0600

    meti otraaaa llinea de codigo
    no pues nada que decir, solo que me siento estresado por todo lo que se viene

--- index.php
+++ index.php
@@ -2,4 +2,6 @@
   echo "¡Hola que tal, esta es una prueba de codigo mediante VS code, yo moifico algo aqui de manera localy todo se sube al repositorio y se actualiza , que chido es esto! PHP está funcionando correctamente.\n";
   echo "La fecha y hora actual es: " . date("Y-m-d H:i:s");
 echo "estamos metiendo mas lineas de codigo para ver como cambia todo";
+
+echo "andamos practicando todo esto aver que tal sale";
 ?>
